cebf95d0552b0b043cf26e1a49d131d168b54d94
Jamal <Jamal_Ali@outlook.com>
2023-12-17 15:08:19 +0200

    Reading from appsettings

--- src/Cool.App.HttpApi.Host/Controllers/HomeController.cs
+++ src/Cool.App.HttpApi.Host/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Volo.Abp.AspNetCore.Mvc;
 
 namespace Cool.App.Controllers;
 
 public class HomeController : AbpController
 {
+    private readonly IConfiguration _configuration;
+
+    public HomeController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public ActionResult Index()
     {
         return Redirect("~/swagger");
@@ -13,6 +20,11 @@ public class HomeController : AbpController
     public ActionResult Test()
     {
         //return BadRequest();
-        return Ok(new { Message = "Works Fine", Id = 10 });
+        return Ok(new
+        {
+            Message = "Works Fine",
+            Id = 10,
+            Value = _configuration.GetValue<string>("CoolKey")
+        });
     }
 }
