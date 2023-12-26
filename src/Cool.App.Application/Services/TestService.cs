91617cf69401a7544aa7d4ee6f75e8f13be2bb65
Jamal <Jamal_Ali@outlook.com>
2023-12-26 14:55:51 +0200

    Submit changes

---
+++ src/Cool.App.Application/Services/TestService.cs
@@ -0,0 +1,31 @@
+﻿using Cool.App.Dtos;
+using System.Threading.Tasks;
+using Volo.Abp.Caching;
+
+namespace Cool.App.Services;
+
+public class TestService : AppAppService
+{
+    private readonly IDistributedCache<CoolCachItem> _cache;
+
+    public TestService(IDistributedCache<CoolCachItem> cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<bool> SetCahceItem(string item, string name, decimal price)
+    {
+        await _cache.SetAsync(item, new CoolCachItem
+        {
+            Name = name,
+            Price = price,
+        });
+        return true;
+    }
+
+    public async Task<CoolCachItem?> GetCahceItem(string item)
+    {
+        var gottenItem = await _cache.GetAsync(item);
+        return gottenItem;
+    }
+}
